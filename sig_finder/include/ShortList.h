15b5312804c417f72cc8de18daacbb049bb50001
hasherezade <hasherezade@gmail.com>
2024-02-14 09:55:48 -0800

    [FEATURE] Keep the ShortList in a sorted form

--- sig_finder/include/ShortList.h
+++ sig_finder/include/ShortList.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdlib>
 
+#define INVALID_INDEX (-1)
+
 template <class Element>
 class ShortList
 {
@@ -14,6 +16,7 @@ public:
 			std::cerr << "Allocating ShortList failed!\n";
 		}
 	}
+
 	~ShortList()
 	{
 		if (this->list) {
@@ -23,25 +26,28 @@ public:
 
 	bool resize(size_t newElementCount)
 	{
+		if (!this->list) { //should not be NULL
+			return false;
+		}
 		this->list = (Element*)::realloc(this->list, newElementCount * sizeof(Element));
 		if (this->list) {
 			this->maxCount = newElementCount;
 			return true;
 		}
+		std::cerr << "Failed to reallocate for the element count: " << std::dec << newElementCount << std::endl;
 		return  false;
 	}
 
 	bool push_back(Element n)
 	{
 		if (elCount >= maxCount) {
-			std::cerr << "Short list overflowed, starting to drop elements:" << maxCount << std::endl;
+			std::cerr << "Short list overflowed, starting to drop elements:" << std::dec << maxCount << std::endl;
 			return false;
 		}
-		if (find(n)) {
-			return true;
+		size_t indx = _getItemIndex(n);
+		if (indx == INVALID_INDEX) {
+			return this->_addItemSorted(n);
 		}
-		list[elCount] = n;
-		elCount++;
 		return true;
 	}
 
@@ -79,6 +85,89 @@ public:
 	}
 
 protected:
+	bool _addItemSorted(Element it)
+	{
+		size_t itIndx = _getItemIndex(it);
+		size_t indx = _findFirstGreater(it, itIndx);
+		if (indx == INVALID_INDEX) {
+			list[this->elCount++] = it;
+			return true;
+		}
+		if (!_shiftItemsRight(indx)) {
+			return false;
+		}
+		list[indx] = it;
+		return true;
+	}
+
+	size_t _findFirstGreater(Element it, size_t startIndx = 0)
+	{
+		if (elCount == 0) {
+			return INVALID_INDEX;
+		}
+		if (startIndx == INVALID_INDEX) {
+			startIndx = 0;
+		}
+		for (size_t i = startIndx; i < elCount; i++)
+		{
+			if (list[i] > it) {
+				return i;
+			}
+		}
+		return elCount;
+	}
+
+	bool _shiftItemsRight(size_t startIndx)
+	{
+		if (startIndx == INVALID_INDEX) {
+			return false;
+		}
+		if (elCount >= maxCount) {
+			return false;
+		}
+		for (size_t i = elCount; i > startIndx; i--) {
+			list[i] = list[i - 1];
+		}
+		elCount++;
+		return true;
+	}
+
+	bool _shiftItemsLeft(int startIndx)
+	{
+		if (startIndx == INVALID_INDEX) {
+			return false;
+		}
+		for (size_t i = startIndx + 1; i < elCount; i++) {
+			list[i - 1] = list[i];
+		}
+		list[ItemCount - 1] = 0;
+		list--;
+		return true;
+	}
+
+	size_t _getItemIndex(Element it)
+	{
+		if (!list || elCount == 0) {
+			return INVALID_INDEX;
+		}
+		size_t start = 0;
+		size_t stop = elCount;
+		while (start < stop) {
+			size_t mIndx = (start + stop) / 2;
+			if (list[mIndx] == it) {
+				return mIndx;
+			}
+			if (list[mIndx] < it) {
+				start = mIndx + 1;
+			}
+			else if (list[mIndx] > it) {
+				stop = mIndx;
+			}
+		}
+		return INVALID_INDEX;
+	}
+
+
 	size_t maxCount;
 	size_t elCount;
 	Element* list;
